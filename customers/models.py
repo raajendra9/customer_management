cddadeaa0cef426fb583b086eaf19f3d0fefef5b
raajendra9 <rajendrareddy.akkala@gmail.com>
2019-12-24 01:11:58 +0530

    model added

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField("First name", max_length=50)
+    last_name = models.CharField("Last name", max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    adress = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateField("Created At", auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
+    
